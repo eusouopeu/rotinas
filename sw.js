b4f51bded3bdd89927980138c4c4659c5a3e5d71
eusouopeu <pedro.caio@ufba.br>
2026-07-07 20:36:50 -0300

    Initial commit: Rotinas PWA
    Add timed routines and markdown notes app with offline support via service worker.

---
+++ sw.js
@@ -0,0 +1,52 @@
+const CACHE_NAME = "rotinas-cache-v11";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./icon-512-maskable.png"];
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).catch(() => {})
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request)
+        .then((response) => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy)).catch(() => {});
+          return response;
+        })
+        .catch(() => caches.match("./index.html"));
+    })
+  );
+});
+
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+  const routineId = event.notification.data && event.notification.data.routineId;
+  event.waitUntil(
+    self.clients.matchAll({ type: "window", includeUncontrolled: true }).then((clientList) => {
+      for (const client of clientList) {
+        if ("focus" in client) {
+          client.focus();
+          client.postMessage({ type: "startRoutine", routineId });
+          return;
+        }
+      }
+      if (self.clients.openWindow) {
+        return self.clients.openWindow("./index.html?start=" + routineId);
+      }
+    })
+  );
+});
