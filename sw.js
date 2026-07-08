7ecf22fc80bf0a0aa708117aaa8851d864a3dd95
eusouopeu <pedro.caio@ufba.br>
2026-07-08 01:59:49 -0300

    Ajusta swipe do botão e incrementa CACHE_NAME do service worker para v14

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "rotinas-cache-v13";
+const CACHE_NAME = "rotinas-cache-v14";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./icon-512-maskable.png"];
 
 self.addEventListener("install", (event) => {
